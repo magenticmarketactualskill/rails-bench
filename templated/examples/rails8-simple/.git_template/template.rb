ee4fd0acc70d0e63dcca98724fb22f580b782f65
Eric Laquer <LaquerEric@gmail.com>
2025-12-19 17:08:13 -0500

    .git_template syntax ok and templated empty

--- templated/examples/rails8-simple/.git_template/template.rb
+++
@@ -1,6 +0,0 @@
-
-
-puts "Generating Gemfile"
-require_relative 'template_parts/template_for__Gemfile'
-TemplateForGemfile0001.generate
-
--- templated/examples/rails8-simple/config/application.rb
+++
@@ -1,9 +0,0 @@
-# Minimal Rails application configuration for template processing
-require_relative "boot"
-require "rails/all"
-
-module TemplateTestApp
-  class Application < Rails::Application
-    config.load_defaults 7.0
-  end
-end
