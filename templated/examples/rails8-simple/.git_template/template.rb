be129a17dbc00e7fd4b33bf1713c33f4ddd38144
Eric Laquer <LaquerEric@gmail.com>
2025-12-19 17:05:34 -0500

    .git_template syntax ok

--- templated/examples/rails8-simple/.git_template/template.rb
+++ templated/examples/rails8-simple/.git_template/template.rb
@@ -1,11 +1,6 @@
-# Template for rails8-simple
-# Updated by git-template
-# Updated: 2025-12-19 11:35:48
 
-puts "Applying rails8-simple template..."
 
-# Simple template that doesn't require Rails
-# Create a simple file to show it's working
-File.write('template_applied.txt at #{Time.now}")
+puts "Generating Gemfile"
+require_relative 'template_parts/template_for__Gemfile'
+TemplateForGemfile0001.generate
 
-puts "rails8-simple template update complete!"
