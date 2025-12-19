72bf63e60af004ac12fa637a34834a259b40abaa
Eric Laquer <LaquerEric@gmail.com>
2025-12-19 06:50:12 -0500

    Kiro run

--- templated/examples/rails8-simple/.git_template/template.rb
+++ templated/examples/rails8-simple/.git_template/template.rb
@@ -4,13 +4,13 @@
 
 # Load generator modules
 
-require_relative '../../../lib/git_template/generator/view.rb'
-require_relative '../../../lib/git_template/generator/test.rb'
-require_relative '../../../lib/git_template/generator/home_feature.rb'
-require_relative '../../../lib/git_template/generator/post_feature.rb'
-require_relative '../../../lib/git_template/generator/completion.rb'
+# require_relative '../../../lib/git_template/generator/view.rb'
+# require_relative '../../../lib/git_template/generator/test.rb'
+# require_relative '../../../lib/git_template/generator/home_feature.rb'
+# require_relative '../../../lib/git_template/generator/post_feature.rb'
+# require_relative '../../../lib/git_template/generator/completion.rb'
 
-say "Applying template..."
+# say "Applying template..."
 
 #~ 010_PHASE_RubyVersion
 # Ruby version configuration (handled by Rails application setup)
@@ -19,15 +19,16 @@ say "Applying template..."
 # Module Usage: GitTemplate::Generators::GemBundle
 # Method: execute()
 
-require_relative '../../../lib/git_template/generator/gem_bundle.rb'
-klass = GitTemplate::Generators::GemBundle
-klass.generate(
-    klass::Config.new(
-        id: "#~ 030_PHASE_GemBundle"
-        groups:[ 
-            GemGroup.new(
-                envs:[:development]
-                gem_text: <<-TEXT
+$LOAD_PATH.unshift(File.expand_path('../../../lib', __FILE__))
+require 'git_template/generator/gem_bundle'
+
+generator = GitTemplate::Generators::GemBundle.new(
+  GitTemplate::Generators::Config.new(
+    id: "#~ 030_PHASE_GemBundle",
+    groups: [ 
+      GitTemplate::Generators::GemGroup.new(
+        envs: [],
+        gem_text: <<-TEXT
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.1.1"
 
@@ -35,18 +36,20 @@ gem "rails", "~> 8.1.1"
 gem "propshaft"
 
 # Use sqlite3 as the database for Active Record
-
 gem "sqlite3", ">= 2.1"
+
 # Use the Puma web server [https://github.com/puma/puma]
-
 gem "puma", ">= 5.0"
-# Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
 
+# Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
 gem "importmap-rails"
+
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "turbo-rails"
+
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem "stimulus-rails"
+
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
@@ -72,16 +75,45 @@ gem "thruster", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
-
 TEXT
-            ],
-            {
-                envs:[:development,:test]
-                gems:[]
-            },
-        ]       
-    )
+      ),
+      GitTemplate::Generators::GemGroup.new(
+        envs: [:development, :test],
+        gem_text: <<-TEXT
+  # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
+  gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
+
+  # Audits gems for known security defects (use config/bundler-audit.yml to ignore issues)
+  gem "bundler-audit", require: false
+
+  # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
+  gem "brakeman", require: false
+
+  # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
+  gem "rubocop-rails-omakase", require: false
+TEXT
+      ),
+      GitTemplate::Generators::GemGroup.new(
+        envs: [:development],
+        gem_text: <<-TEXT
+  # Use console on exceptions pages [https://github.com/rails/web-console]
+  gem "web-console"
+TEXT
+      ),
+      GitTemplate::Generators::GemGroup.new(
+        envs: [:test],
+        gem_text: <<-TEXT
+  # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
+  gem "capybara"
+  gem "selenium-webdriver"
+TEXT
+      )
+    ]       
+  )
 )
+
+generator.generate
+=begin
 #GitTemplate::Generators::GemBundle.execute
 
 #~ 040_PHASE_View
@@ -120,3 +152,5 @@ say "#~ 900_PHASE_Complete"
 GitTemplate::Generators::Completion.execute
 
 say "Template application completed!", :green
+
+=end
